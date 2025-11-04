57dbd048f06a8e9ff1e9dbb7887325d5b202225a
DESKTOP-04NRI2O\82104 <hyunook4929@gmail.com>
2025-11-04 21:36:08 +0900

    feat: Implement optimistic UI update for like button UX

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,14 +1,36 @@
 import { SimplePost } from "@/models/post";
 import useSWR, { useSWRConfig } from "swr";
 
+async function updateLike(id: string, isLike: boolean) {
+  return fetch("/api/likes", {
+    method: "PUT",
+    body: JSON.stringify({ id, isLike }),
+  }).then((res) => res.json());
+}
+
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>("/api/posts");
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>("/api/posts");
+
   const setLike = (post: SimplePost, username: string, isLike: boolean) => {
-    fetch("/api/likes", {
-      method: "PUT",
-      body: JSON.stringify({ id: post.id, isLike }),
-    }).then(() => mutate("/api/posts"));
+    const newPost = {
+      ...post,
+      likes: isLike
+        ? [...post.likes, username]
+        : post.likes.filter((item) => item !== username),
+    };
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, isLike), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
   };
 
   return { posts, isLoading, error, setLike };
